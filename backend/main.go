eb082369e1621a5adb773830c5dd3cc0d0bf3a97
Precious Okwu <libracoder@gmail.com>
2024-08-22 11:59:39 +0100

    update function

--- backend/main.go
+++ backend/main.go
@@ -2,29 +2,42 @@ package main
 
 import (
 	"fmt"
-	"github.com/labstack/echo/v4"
 	"net/http"
 	"os"
-)
 
-type User struct {
-	Name string
-	Age  int
-}
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
 	e := echo.New()
 
-	var users []User
-	users = append(users, User{Name: "Precious", Age: 10})
-	users = append(users, User{Name: "Ijeoma", Age: 20})
-
 	e.GET("/", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, users)
+		envVars := make(map[string]string)
+		for _, env := range os.Environ() {
+			pair := splitAtFirstEqual(env)
+			envVars[pair[0]] = pair[1]
+		}
+
+		response := map[string]interface{}{
+			"message": "These are your current environment variables. Update an env and refresh.",
+			"envs":    envVars,
+		}
+
+		return c.JSON(http.StatusOK, response)
 	})
+
 	port := fmt.Sprintf(":%s", os.Getenv("PORT"))
 	fmt.Printf("port is %v", port)
 	e.Logger.Fatal(e.Start(port))
 }
 
-//nixpacks build --name mn .  --install-cmd  "go mod download"  --build-cmd "go build -o out backend/main.go"
+// splitAtFirstEqual splits a string at the first occurrence of '='
+// and returns a slice with the key and value as separate elements.
+func splitAtFirstEqual(s string) []string {
+	for i := range s {
+		if s[i] == '=' {
+			return []string{s[:i], s[i+1:]}
+		}
+	}
+	return []string{s, ""}
+}
